4901e6c2c3964a88e7405f3b8d4e5dd7b76b97c5
BorjaLia <borja.lia.deichler@gmail.com>
2026-09-20 20:24:17 -0300

    Add Bubble

--- Assets/Scripts/Algorithm/Algorithm.cs
+++ Assets/Scripts/Algorithm/Algorithm.cs
@@ -114,6 +114,55 @@ namespace CustomMath
 
         #endregion
         
+        #region Bubble Sort
+
+        /*
+         *  Costo computacional
+         *  Mejor:      O(n)
+         *  Promedio:   O(n^2)
+         *  Peor:       O(n^2)
+         *  
+         *  Complejidad espacial:   O(1) auxiliar
+         */
+
+        /*
+         *  Bubble Sort recorre repetidamente el array, 
+         *  compara elementos adyacentes y los intercambia 
+         *  si están en el orden equivocado. 
+         *  El proceso se repite hasta que no se necesitan más intercambios.
+         *  
+         */
+
+        public static void BubbleSort<T>(T[] array) where T : IComparable<T>
+        {
+            if (array == null || array.Length <= 1) return;
+
+            int n = array.Length;
+            bool swapped;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                swapped = false;
+
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (array[j].CompareTo(array[j + 1]) > 0)
+                    {
+                        T temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+
+                        swapped = true;
+                    }
+                }
+
+                if (!swapped)
+                    break;
+            }
+        }
+
+        #endregion
+        
         #region Bitonic
 
         /*
